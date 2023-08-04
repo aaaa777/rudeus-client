24d367c4d16190520a6ee50ffc3a15ff0b01130e
aaaa777 <aaaa777@users.noreply.github.com>
2023-08-04 18:16:19 +0900

    プロジェクト ファイルを追加します。

---
+++ rudeus-client/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace rudeus_client;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ rudeus-client/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace rudeus_client;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
