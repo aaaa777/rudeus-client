6331bd6607820b0aab310e9846db02911fb148b2
aaaa777 <aaaa777@users.noreply.github.com>
2023-08-13 07:07:39 +0900

    Background実行用のサービスを作成

---
+++ RudeusBg/Program.cs
@@ -0,0 +1,10 @@
+using RudeusBg;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
