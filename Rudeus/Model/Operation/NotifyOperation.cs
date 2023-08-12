4e60ff7e5fb0f3bfb301c435ab461f965eb7c8ba
aaaa777 <aaaa777@users.noreply.github.com>
2023-08-12 15:44:09 +0900

    サーバのpushレスポンスを実行するOperationクラスを作成

---
+++ Rudeus/Model/Operation/NotifyOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rudeus.Model.Operation
+{
+    internal class NotifyOperation : Operation
+    {
+        public string Opcode = "Notify";
+        
+        public void Start()
+        {
+            Console.WriteLine($"{Opcode} executed");
+        }
+    }
+}
