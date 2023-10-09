199e54f0085a6bd124f8c48c37757e285abb0cff
aaaa777 <aaaa777@users.noreply.github.com>
2023-10-10 02:05:23 +0900

    AutoUpdaterの追加

---
+++ RudeusBg/BgUpdater.cs
@@ -0,0 +1,15 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rudeus.Model
+{
+    internal class BgUpdater
+    {
+        public static void Run()
+        {
+            AutoUpdater.Start();
+        }
+    }
+}
---
+++ RudeusSharedLib/Model/Updater.cs
@@ -0,0 +1,22 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rudeus.Model
+{
+    internal class Updater
+    {
+        static public string UpdateCheckUrl = "";
+        
+        static public void Init()
+        {
+            //AutoUpdater;
+        }
+
+        public static void Run()
+        {
+            AutoUpdater.Start(UpdateCheckUrl);
+        }
+    }
+}
