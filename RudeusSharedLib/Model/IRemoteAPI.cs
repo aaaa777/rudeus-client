6236e030fed0b2989243b6cf863421c14633d306
aaaa777 <aaaa777@users.noreply.github.com>
2023-11-08 06:22:50 +0900

    fix typo

--- RudeusSharedLib/Model/IRemoteAPI.cs
+++ RudeusSharedLib/Model/IRemoteAPI.cs
@@ -8,7 +8,7 @@ namespace Rudeus.Model
 {
     internal interface IRemoteAPI
     {
-        public abstract static RegisterRequest RegisterDevice(string deviceId, string hostname);
+        public abstract static RegisterResponse RegisterDevice(string deviceId, string hostname);
         public abstract static UpdateResponse UpdateDevice(string accessToken, string? hostname = null);
         public abstract static LoginResponse LoginDevice(string accessToken, string userId);
 
