6d7f18960e01b1266030d7ed8712a87185b6e9bd
aaaa777 <aaaa777@users.noreply.github.com>
2023-11-29 04:53:04 +0900

    APIインターフェースクラスに注意書きを追加

---
+++ RudeusSharedLib/API/ICallbackAPI.cs
@@ -0,0 +1,8 @@
+﻿namespace Rudeus.Model
+{
+    internal interface ICallbackAPI
+    {
+        // 移植予定
+        //public abstract static Task<string> ReceiveStudentIdAsync();
+    }
+}
--- RudeusSharedLib/API/IRemoteAPI.cs
+++ RudeusSharedLib/API/IRemoteAPI.cs
@@ -8,6 +8,8 @@ namespace Rudeus.Model
 {
     internal interface IRemoteAPI
     {
+        // メソッドは必ず例外処理を行うこと
+        // 開発時以外はサーバ起因の例外しか発生しないので、基本はリトライ処理だけでいい
         public abstract static RegisterResponse RegisterDevice(string deviceId, string hostname);
         public abstract static UpdateResponse UpdateDevice(string accessToken, string? hostname = null);
         public abstract static LoginResponse LoginDevice(string accessToken, string userId);
