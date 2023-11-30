e57576ca0c31c0126f3b1a9b454808e905943b89
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-11-30 16:46:10 +0900

    IUtilsの修正

--- RudeusSharedLib/IUtils.cs
+++ RudeusSharedLib/IUtils.cs
@@ -2,6 +2,24 @@
 {
     public interface IUtils
     {
-        // public static readonly required string WebPortalUrl;
+        public abstract string ConcatStudentNumberFromMail(string mailAddress);
+
+        public abstract bool IsStudentMailAddress(string mailAddress);
+
+        public abstract string GetPhysicalRamInfo();
+
+        public abstract string[] GetStorageDeviceIdList();
+
+        public abstract static string GetDeviceId();
+
+        public abstract static string GetHostname();
+
+        public abstract static string GetWinVersion();
+
+        public abstract static void RegisterDeviceAndSetData();
+
+        public abstract static void CopyDirectory(string sourceDir, string destDir);
+
+        public abstract static int CompareVersionString(string a, string b, int depth);
     }
 }
