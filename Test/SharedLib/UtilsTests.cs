8fe913e29ba240497e52ce8891330f3e2e68530c
aaaa777 <aaaa777@users.noreply.github.com>
2023-12-04 14:00:46 +0900

    テストケース変更

--- Test/SharedLib/UtilsTests.cs
+++ Test/SharedLib/UtilsTests.cs
@@ -11,35 +11,61 @@ namespace Rudeus.Test
     public class UtilsTests
 
     {
-        [Fact(DisplayName = "IsStudentMailAddress")]
-        public void TestIsStudentMailAddress()
+        /// <summary>
+        /// 学生のメール -> true
+        /// </summary>
+        [Fact(DisplayName = "IsStudentMailAddress(): s1234567@s.do-johodai.ac.jp -> True")]
+        public void TestIsStudentMailAddress1()
         {
-
-            // 学生のメール -> true
             Utils.IsStudentMailAddress("s1234567@s.do-johodai.ac.jp").Is(true);
+        }
 
-            // 教員のメール -> false
+        /// <summary>
+        /// 教員のメール -> false
+        /// </summary>
+        [Fact(DisplayName = "IsStudentMailAddress(): samplename@s.do-johodai.ac.jp -> False")]
+        public void TestIsStudentMailAddress2()
+        {
             Utils.IsStudentMailAddress("samplename@s.do-johodai.ac.jp").Is(false);
+        }
 
-            // 外部のメール -> false
+        /// <summary>
+        /// 外部のメール -> false
+        /// </summary>
+        [Fact(DisplayName = "IsStudentMailAddress(): s1234567@www.example.com -> False")]
+        public void TestIsStudentMailAddress3()
+        {
             Utils.IsStudentMailAddress("s1234567@www.example.com").Is(false);
         }
 
-        [Fact(DisplayName = "ConcatStudentNumberFromMail")]
+
+
+        [Fact(DisplayName = "ConcatStudentNumberFromMail(): \"s1234567@s.do-johodai.ac.jp\" -> \"1234567\"")]
         public void TestConcatStudentNumberFromMail()
         {
             Utils.ConcatStudentNumberFromMail("s1234567@s.do-johodai.ac.jp").Is("1234567");
         }
 
-        [Fact(DisplayName = "CompareVersionString")]
-        public void TestCompareVersionString()
+
+
+        [Fact(DisplayName = "CompareVersionString(): \"1.0.0\", \"1.0.0\" -> 0")]
+        public void TestCompareVersionString1()
         {
             // 引数1 == 引数2 -> 0
             Utils.CompareVersionString("1.0.0", "1.0.0").Is(0);
+        }
+
+        [Fact(DisplayName = "CompareVersionString(): \"1.0.0\", \"0.32.0\" -> 1")]
+        public void TestCompareVersionString2()
+        {
 
             // 引数1 > 引数2 -> 1
             Utils.CompareVersionString("1.0.0", "0.32.0").Is(1);
+        }
 
+        [Fact(DisplayName = "CompareVersionString(): \"1.0.0\", \"2.1.0\" -> -1")]
+        public void TestCompareVersionString()
+        {
             // 引数1 < 引数2 -> -1
             Utils.CompareVersionString("1.0.0", "2.1.0").Is(-1);
         }
