51da50d6498d6c9a7f034444e46243ee3aedfdbf
aaaa777 <aaaa777@users.noreply.github.com>
2023-12-04 09:07:05 +0900

    ファイル名の修正

--- Application/Properties/Resources.Designer.cs
+++ Application/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormApp.Properties {
+namespace Application.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinFormApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Application.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
