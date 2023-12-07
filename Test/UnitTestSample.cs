97ec815109de14e2acff5607ab9d06f214bbd3e0
aaaa777 <aaaa777@users.noreply.github.com>
2023-12-07 10:31:12 +0900

    サンプルテスト

--- Test/UnitTestSample.cs
+++ Test/UnitTestSample.cs
@@ -2,11 +2,28 @@ namespace Test
 {
     public class UnitTestSample
     {
-        [Fact(DisplayName = "テストの書き方サンプル")]
-        public void TestTest()
+        // 単体テストのサンプル
+        [Fact(DisplayName = "abs(1) -> 1")]
+        public void TestAbs1()
         {
-            (1 + 1).Is(2);
+            Abs(1).Is(1);
         }
 
+        [Fact(DisplayName = "abs(-12) -> 12")]
+        public void TestAbs2()
+        {
+            Abs(-12).Is(12);
+        }
+
+        [Fact(DisplayName = "abs(0) -> 0")]
+        public void TestAbs3()
+        {
+            Abs(0).Is(0);
+        }
+
+        private int Abs(int num)
+        {
+            return num < 0 ? -num : num;
+        }
     }
 }
