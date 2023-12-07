29838ad75107d8719a60ed98608bb52a81c95305
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-12-07 15:03:01 +0900

    Commandにバージョニングを設定

---
+++ Command/Version.cs
@@ -0,0 +1,12 @@
+﻿using SharedLib;
+
+namespace Rudeus.Command
+{
+    public class Version : IVersion
+    {
+        public static new string ToString() 
+        {
+            return "0.1.7";
+        }
+    }
+}
